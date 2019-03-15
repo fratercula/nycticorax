1714fed0ca014dabfafc7ac2968c49fb16ab07bd
LoeiFy <LoeiFy@gmail.com>
2019-03-15 20:10:01 +0800

    docs: demo update

--- falco.config.js
+++ falco.config.js
@@ -1,5 +1,16 @@
 const { NODE_ENV } = process.env
 
+const output = {}
+
+if (NODE_ENV === 'docs') {
+  output.filename = 'index.[hash:8].js'
+}
+
+if (NODE_ENV === 'umd') {
+  output.library = 'nycticorax'
+  output.libraryTarget = 'umd'
+}
+
 module.exports = {
   externals: [
     {
@@ -8,22 +19,15 @@ module.exports = {
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react',
-      // urls: 'https://cdn.staticfile.org/react/16.8.2/umd/react.development.js',
       urls: 'https://cdn.staticfile.org/react/16.8.2/umd/react.production.min.js',
     },
     {
       name: 'react-dom',
       root: 'ReactDOM',
       urls: 'https://cdn.staticfile.org/react-dom/16.8.2/umd/react-dom.production.min.js',
-      // urls: 'https://cdn.staticfile.org/react-dom/16.8.2/umd/react-dom.development.js',
     },
   ],
   registry: 'https://registry.npm.taobao.org',
   mode: NODE_ENV ? 'production' : 'development',
-  output: NODE_ENV === 'prod'
-    ? {
-      library: 'nycticorax',
-      libraryTarget: 'umd',
-    }
-    : {},
+  output,
 }
