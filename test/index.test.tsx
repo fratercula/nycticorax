f490379895ab1562b1f254ae7939ebd35f6d16d6
loeify <loeify@gmail.com>
2021-07-05 23:04:40 +0800

    test: hook

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,34 +1,49 @@
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { mount } from 'enzyme'
 import Nycticorax from '../src'
 
-type Store = { name: string, age: number }
+type Store = { name: string, age: number, other: boolean }
 
 const nycticorax = new Nycticorax<Store>()
 
 const {
   createStore,
-  // dispatch,
+  dispatch,
   // connect,
   useStore,
 } = nycticorax
 
 // type Connect = CT<Store>
 
-createStore({ name: 'xyz', age: 1 })
+createStore({ name: 'xyz', age: 1, other: false })
 
 describe('nycticorax', () => {
-  it('hook', () => {
+  it('hook', async () => {
     function Hook() {
       const { name, age } = useStore('name', 'age')
       return (
         <div>
-          <div>{name}</div>
-          <div>{age}</div>
+          <div className="name">{name}</div>
+          <div className="age">{age}</div>
         </div>
       )
     }
     const wrapper = mount(<Hook />)
-    expect(wrapper.render()).toMatchSnapshot()
+    let name = wrapper.findWhere((node) => node.text() === 'xyz' && node.hasClass('name'))
+    expect(name).toHaveLength(1)
+
+    const age = wrapper.findWhere((node) => node.text() === '1' && node.hasClass('age'))
+    expect(age).toHaveLength(1)
+
+    act(() => {
+      dispatch({ name: 'abc' }, true)
+      dispatch({ other: true }, true)
+    })
+
+    name = wrapper.findWhere((node) => node.text() === 'abc' && node.hasClass('name'))
+    expect(name).toHaveLength(1)
+
+    // expect(wrapper.render()).toMatchSnapshot()
   })
 })
